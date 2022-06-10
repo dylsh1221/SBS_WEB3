a12e6bd0c6533ed85228d9acf0835f62db827223
dylsh1221 <dylsh1221@naver.com>
2022-06-10 20:56:59 +0900

    1일차

---
+++ 1day/main4.js
@@ -0,0 +1,18 @@
+// j-Query
+
+/* 
+    자바스크립트 기본 언어들의 코드 길이가 너무 길어서 이걸 짧게하고 싶어서 짧게 쓰는 코드를 발명하고
+    그 코드들만 있으면 자바스크립트를 짧게 쓸 수가 있다고 해서  그걸 대중들에게 공개했습니다.
+    jQuery 라는 이름을 붙여서.. 그게 바로 
+
+    웹본 파일 버전
+    원본 파일 코드의 공백을 제거한 버전
+    몇가지 기능이 빠진 버전
+    몇가지 기능이 빠진 코드의 공백
+*/
+
+// document.getElementById("hello").innerHTML = "반갑습니다."
+
+// $("#hello").html("안녕?!")
+// $("#hello").css("color","hotpink")
+$("#hello").attr("href","https://www.naver.com")
